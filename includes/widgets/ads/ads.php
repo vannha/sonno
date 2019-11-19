e72f4615ffddf525e33e58422679aa97662acf08
vannha <phongnhatk21@gmail.com>
2019-11-19 09:11:04 +0700

    commit -

--- includes/widgets/ads/ads.php
+++ includes/widgets/ads/ads.php
@@ -3,66 +3,71 @@
  * ThemeSquared: Ads Images
  * Display Ads Images in siderbr
  *
- * @package		SpyroPress
- * @category	Widgets
- * @author		SpyroSol
+ * @package        SpyroPress
+ * @category    Widgets
+ * @author        SpyroSol
  */
+if ( ! function_exists( 'register_cpt_widget' ) ) {
+	return;
+}
 
 class Spyropress_Widget_Ads extends SpyropressWidget {
 
-    /**
-     * Constructor
-     */
-    function __construct() {
+	/**
+	 * Constructor
+	 */
+	function __construct() {
 
-        // Widget variable settings.
-        $this->path = get_template_directory() . '/includes/widgets/ads';
-        $this->cssclass = 'widget_ads';
-        $this->description = esc_html__( 'Display Ads Images in siderbr.', 'sonno' );
-        $this->id_base = 'spyropress_ads';
-        $this->name = esc_html__( 'ThemeSquared: Ads Images', 'sonno' );
+		// Widget variable settings.
+		$this->path        = get_template_directory() . '/includes/widgets/ads';
+		$this->cssclass    = 'widget_ads';
+		$this->description = esc_html__( 'Display Ads Images in siderbr.', 'sonno' );
+		$this->id_base     = 'spyropress_ads';
+		$this->name        = esc_html__( 'ThemeSquared: Ads Images', 'sonno' );
 
-        $this->fields = array(
+		$this->fields = array(
 
-            array(
-                'label' => esc_html__( 'Title', 'sonno' ),
-                'id' => 'spyropress_title',
-                'type' => 'text',
-            ),
-            array(
-                'label' => esc_html__( 'Ads', 'sonno' ),
-                'type' => 'repeater',
-                'id' => 'spyropress_ads',
-                'hide_label' => true,
-                'fields' => array(
-                
-                    array(
-                        'label' => esc_html__( 'Image', 'sonno' ),
-                        'id' => 'spyropress_image',
-                        'type' => 'upload',
-                    ),
-                    
-                    array(
-                        'label' => esc_html__( 'Url / Link', 'sonno' ),
-                        'id' => 'spyropress_url',
-                        'type' => 'text',
-                    )
-                )
-            )
-        );
+			array(
+				'label' => esc_html__( 'Title', 'sonno' ),
+				'id'    => 'spyropress_title',
+				'type'  => 'text',
+			),
+			array(
+				'label'      => esc_html__( 'Ads', 'sonno' ),
+				'type'       => 'repeater',
+				'id'         => 'spyropress_ads',
+				'hide_label' => true,
+				'fields'     => array(
 
-        $this->create_widget();
-    }
+					array(
+						'label' => esc_html__( 'Image', 'sonno' ),
+						'id'    => 'spyropress_image',
+						'type'  => 'upload',
+					),
 
-    function widget( $spyropress_args, $spyropress_instance ) {
+					array(
+						'label' => esc_html__( 'Url / Link', 'sonno' ),
+						'id'    => 'spyropress_url',
+						'type'  => 'text',
+					)
+				)
+			)
+		);
 
-        // extracting info
-        extract( $spyropress_args );extract( $spyropress_instance );
+		$this->create_widget();
+	}
 
-        // get view to render
-        require( $this->get_view() );
-    }
+	function widget( $spyropress_args, $spyropress_instance ) {
 
-} 
+		// extracting info
+		extract( $spyropress_args );
+		extract( $spyropress_instance );
+
+		// get view to render
+		require( $this->get_view() );
+	}
+
+}
+
 // class Spyropress_Widget_Ads
-register_widget( 'Spyropress_Widget_Ads' );
+register_cpt_widget( 'Spyropress_Widget_Ads' );
--- includes/widgets/recent-posts/recent-posts.php
+++ includes/widgets/recent-posts/recent-posts.php
@@ -3,52 +3,57 @@
  * ThemeSquared: Recent Posts
  * The most recent posts on your site.
  *
- * @package		SpyroPress
- * @category	Widgets
- * @author		SpyroSol
+ * @package        SpyroPress
+ * @category    Widgets
+ * @author        SpyroSol
  */
+if ( ! function_exists( 'register_cpt_widget' ) ) {
+	return;
+}
 
 class Spyropress_Widget_Recent_Posts extends SpyropressWidget {
 
-    /**
-     * Constructor
-     */
-    function __construct() {
+	/**
+	 * Constructor
+	 */
+	function __construct() {
 
-        // Widget variable settings.
-        $this->path = get_template_directory() . '/includes/widgets/recent-posts';
-        $this->cssclass = 'widget_popular_posts';
-        $this->description = esc_html__( 'The most recent posts on your site.', 'sonno' );
-        $this->id_base = 'spyropress_recent_posts';
-        $this->name = esc_html__( 'ThemeSquared: Recent Posts', 'sonno' );
+		// Widget variable settings.
+		$this->path        = get_template_directory() . '/includes/widgets/recent-posts';
+		$this->cssclass    = 'widget_popular_posts';
+		$this->description = esc_html__( 'The most recent posts on your site.', 'sonno' );
+		$this->id_base     = 'spyropress_recent_posts';
+		$this->name        = esc_html__( 'ThemeSquared: Recent Posts', 'sonno' );
 
-        $this->fields = array(
+		$this->fields = array(
 
-            array(
-                'label' => esc_html__( 'Title', 'sonno' ),
-                'id' => 'spyropress_title',
-                'type' => 'text',
-            ),
-            array(
-                'label' => esc_html__( 'Number of posts to show:', 'sonno' ),
-                'id' => 'spyropress_number',
-                'type' => 'range_slider',
-                'max' => 50
-            )
-        );
+			array(
+				'label' => esc_html__( 'Title', 'sonno' ),
+				'id'    => 'spyropress_title',
+				'type'  => 'text',
+			),
+			array(
+				'label' => esc_html__( 'Number of posts to show:', 'sonno' ),
+				'id'    => 'spyropress_number',
+				'type'  => 'range_slider',
+				'max'   => 50
+			)
+		);
 
-        $this->create_widget();
-    }
+		$this->create_widget();
+	}
 
-    function widget( $spyropress_args, $spyropress_instance ) {
+	function widget( $spyropress_args, $spyropress_instance ) {
 
-        // extracting info
-        extract( $spyropress_args );extract( $spyropress_instance );
+		// extracting info
+		extract( $spyropress_args );
+		extract( $spyropress_instance );
 
-        // get view to render
-        require( $this->get_view() );
-    }
+		// get view to render
+		require( $this->get_view() );
+	}
 
-} 
+}
+
 // class SpyroPress_Widget_TabWidget
-register_widget( 'Spyropress_Widget_Recent_Posts' );
+register_cpt_widget( 'Spyropress_Widget_Recent_Posts' );

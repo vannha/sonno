67d9dea97e9a1b020fbc30a2d22f028bb9e5f1be
vannha <phongnhatk21@gmail.com>
2019-12-20 15:09:45 +0700

    commit -

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 </head>
 <body <?php body_class(); ?>>
 <?php 
-$aadadsdads;
+die($aadadsdads;);
     spyropress_wrapper(); 
         spyropress_before_header(); 
         if( !get_setting( 'page-loader' ) ):
